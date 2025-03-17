2f995d47c220e53a7fd90a228f97dc8aee4605e8
nuestraerareal <92056253+nuestraerareal@users.noreply.github.com>
2025-03-16 18:29:36 -0600

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,17 +1,12 @@
 // https://github.com/nuxt-themes/docus/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   docus: {
-    title: 'Docus',
+    title: 'DicSur',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
     socials: {
-      twitter: 'nuxt_js',
-      github: 'nuxt-themes/docus',
-      nuxt: {
-        label: 'Nuxt',
-        icon: 'simple-icons:nuxtdotjs',
-        href: 'https://nuxt.com'
-      }
+      twitter: 'theunusher',
+      github: 'nuestraerareal',
     },
     github: {
       dir: '.starters/default/content',
