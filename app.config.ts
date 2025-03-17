a59b0fb5726b74e23c7e1f62bcdf6f75a0f427b0
nuestraerareal <92056253+nuestraerareal@users.noreply.github.com>
2025-03-16 18:40:45 -0600

    10 changes by Nuxt Studio

--- app.config.ts
+++ app.config.ts
@@ -4,10 +4,12 @@ export default defineAppConfig({
     title: 'DicSur',
     description: 'The best place to start your documentation.',
     image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+
     socials: {
       twitter: 'theunusher',
       github: 'nuestraerareal',
     },
+
     github: {
       dir: '.starters/default/content',
       branch: 'main',
@@ -15,20 +17,31 @@ export default defineAppConfig({
       owner: 'nuxt-themes',
       edit: true
     },
+
     aside: {
       level: 0,
       collapsed: false,
       exclude: []
     },
+
     main: {
       padded: true,
       fluid: true
     },
+
     header: {
       logo: true,
       showLinkIcon: true,
       exclude: [],
       fluid: true
+    },
+
+    footer: {
+      credits: {
+        icon: '',
+        text: 'El diccionario libre.',
+        href: ''
+      }
     }
   }
 })
