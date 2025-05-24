ca864311c72a014205bb2f5d7b8f0921acab51f7
nuestraerareal <92056253+nuestraerareal@users.noreply.github.com>
2025-05-24 10:18:57 -0600

    Create SaludoHora.vue

---
+++ components/SaludoHora.vue
@@ -0,0 +1,11 @@
+<template>
+  <p>{{ saludo }}</p>
+</template>
+
+<script setup lang="ts">
+const hora = new Date().getHours()
+const saludo =
+  hora < 12 ? '¡Buenos días!' :
+  hora < 19 ? '¡Buenas tardes!' :
+  '¡Buenas noches!'
+</script>
