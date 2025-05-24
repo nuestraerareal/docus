2b38657c739da03c609a27b955d3935062ddffe0
nuestraerareal <92056253+nuestraerareal@users.noreply.github.com>
2025-05-23 20:33:01 -0600

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ export default defineAppConfig({
   docus: {
     title: 'DiMex',
     description: 'El diccionario de los mexicanos.',
-    image: 'https://user-images.githubusercontent.com/904724/185365452-87b7ca7b-6030-4813-a2db-5e65c785bf88.png',
+    image: 'https://github.com/nuestraerareal/docus/blob/cfeb981e43619eb6373cf70ffc10585217920fa2/public/cover.png',
 
     socials: {
       twitter: 'theunusher',
