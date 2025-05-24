18559c8a876e4232e42602739b6fecb10682ba5a
nuestraerareal <92056253+nuestraerareal@users.noreply.github.com>
2025-05-24 10:08:50 -0600

    Create Greeting.vue

---
+++ components/Greeting.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ greeting }}</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  computed: {
+    greeting(): string {
+      const hour = new Date().getHours()
+      if (hour < 12) return 'Buenos días.'
+      if (hour < 18) return 'Buenas tardes.'
+      return 'Buenas noches.'
+    }
+  }
+})
+</script>
